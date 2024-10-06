4f3d15a17214df88dc86b5377c98d539e60f9243
kim <kyoungbin1996@gmail.com>
2024-10-06 15:39:37 +0900

    udpate: unionfind

--- unionfFind.py
+++ unionfFind.py
@@ -13,3 +13,27 @@ def union(a, b):
         p[b] = a
     else:
         p[a] = b
+        
+
+# UnionFind
+class UnionFind:
+    def __init__(self, n):
+        self.p = list(range(n))
+        self.r = [0] * n # rank 그 노드가 루트인 트리의 높이
+
+    def find(self, x):
+        if self.p[x] != x:
+            self.p[x] = self.find(self.p[x])
+        return self.p[x]
+
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            if self.r[rootX] > self.r[rootY]:
+                self.p[rootY] = rootX
+            elif self.r[rootX] < self.r[rootY]:
+                self.p[rootX] = rootY
+            else:
+                self.p[rootY] = rootX
+                self.r[rootX] += 1
