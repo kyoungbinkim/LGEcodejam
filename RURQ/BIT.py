ee41f75586fa33450a391b4696a658e9fad2f556
kim <kyoungbin1996@gmail.com>
2024-10-11 15:38:46 +0900

    fix : range Update range Query

--- RURQ/BIT.py
+++ RURQ/BIT.py
@@ -1,5 +1,5 @@
 
-
+# for range update range query
 class BIT:
     def __init__(self, n, leafs):
         self.n = n
@@ -14,7 +14,6 @@ class BIT:
         s = 0  
         idx = idx + 1
         while idx > 0:
-
             s += tree[idx]
             idx -= idx & (-idx)
         return s
@@ -32,21 +31,24 @@ class BIT:
     def updateRange(self, val, l, h) -> None:
     
         self.__updateBit(self.bit1, l, val)
-        self.__updateBit(self.bit2, h + 1, -val)
+        self.__updateBit(self.bit1, h + 1, -val)
     
         self.__updateBit(self.bit2, l, val * (l - 1))
         self.__updateBit(self.bit2, h + 1, -val * h)
         
     # sum of arr[l..h]
     def rangeSum(self, l, h) -> int:
+        if l <= 0:
+            return self.__summation(h) - self.__summation(l-1) + self.acc[h]
         return self.__summation(h) - self.__summation(l-1) + self.acc[h] - self.acc[l-1]
 
 
 def test():
     bit = BIT(12, [1,2,3,4,5,6,1,2,3,4,5,6])
-    print("rangeSum(1, 5) : ", bit.rangeSum(1, 5))
+    print("rangeSum(1, 5) : ", bit.rangeSum(0, 5))
     print("rangeSum(3, 9) : ", bit.rangeSum(3, 9))
     
-    bit.updateRange(3, 1, 5)
-    print("rangeSum(1, 5) : ", bit.rangeSum(1, 5))
+    bit.updateRange(3, 1, 4)
+    for i in range(1,11):
+        print(bit.rangeSum(i, i+1), end = ' ')    
 test()
