2ef0931dff1292338e8e65019b327b4a31815e03
kim <kyoungbin1996@gmail.com>
2025-06-03 11:54:29 +0900

    fix: kosaraju

--- SCC/kosaraju.py
+++ SCC/kosaraju.py
@@ -55,4 +55,58 @@ for _ in range(e):
 
 scc = kosaraju(v, g, revg)
 print(scc)
-            
+
+
+
+'''
+2 번째 방법
+'''
+
+from collections import defaultdict
+class Graph:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = defaultdict(list)
+
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+
+    def dfs(self, v, visited, stack):
+        visited[v] = True
+        for i in self.graph[v]:
+            if not visited[i]:
+                self.dfs(i, visited, stack)
+        stack.append(v)
+
+    def reverseGraph(self):
+        g = Graph(self.V)
+        for i in self.graph:
+            for j in self.graph[i]:
+                g.addEdge(j, i)
+        return g
+
+    def getScc(self):
+        stack = []
+        visited = [False] * (self.V)
+        for i in range(self.V):
+            if not visited[i]:
+                self.dfs(i, visited, stack)
+
+        gr = self.reverseGraph()
+
+        visited = [False] * (self.V)
+        sccs = []
+        while stack:
+            i = stack.pop()
+            if not visited[i]:
+                scc = []
+                gr.dfsUtil(i, visited, scc)
+                sccs.append(scc)
+        return sccs
+
+    def dfsUtil(self, v, visited, scc):
+        visited[v] = True
+        scc.append(v)
+        for i in self.graph[v]:
+            if not visited[i]:
+                self.dfsUtil(i, visited, scc)
